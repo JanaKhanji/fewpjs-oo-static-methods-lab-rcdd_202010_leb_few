ddb9391bcd1e954b7d56a3b09a396b5d2c45c1e6
Jana Khanji <jana.khanji@ieee.org>
2020-12-13 10:51:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ static sanitize(string){
 }
 
 static titleize(string){
-  const dontCap =["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+  const dontCap =['the', "a", "an", "but", "of", "and", "for", "at", "by", "from"];
   let inputArr= string.split(" ")
   let output= inputArr.map((el)=>{
-    dontCap.forEach((item)=>{
-      if (item!==el){
-        el=this.capitalize(el)
-      }
-        }) 
-        return el
+    if(dontCap.find(item => item==el)==undefined){
+     return capitalize(el)
+    }
+    else{
+      return el
+    }
   })
   return output.join(" ")
 }
