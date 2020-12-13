f8f1162e177be4aa5ebaf4bcbbcb6cf49a09880d
Jana Khanji <jana.khanji@ieee.org>
2020-12-13 10:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ static sanitize(string){
    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
 }
 
-static titleize
+static titleize(string){
+  const dontCap =["the"; "a"; "an"; "but"; "of"; "and"; "for"; "at"; "by"; "from"];
+  
+  
+}
 
 }
